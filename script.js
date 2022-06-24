bd0ca214ba7c49b724d89917e0b75607c6bc56d4
Class and object <105230545+Beckyzest@users.noreply.github.com>
2022-06-24 16:02:06 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+var my_name = "My name is Benson Rebecca Funmilola."
+alert(name);
+var height = "I'm 5 feet and 6 inches tall."
+var country = "I am from Nigeria."
+
+document.write('Name : ' + my_name + '<br>' + ' Height : ' + height +' <br> '+ 'Country : ' + country);
+alert(countryname);
+
+
